3694dbdb1383371b25a22fff1131e3feb342cb8c
Ethan M <ethanmdev@gmail.com>
2020-10-09 14:23:44 -0600

    horizontal picker view and playing with ui styles

--- voice MessagesExtension/MessagesViewController.swift
+++ voice MessagesExtension/MessagesViewController.swift
@@ -11,6 +11,34 @@ import Messages
 
 var generator = Generate()
 
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
+
 class MessagesViewController: MSMessagesAppViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextViewDelegate {
     
     @IBOutlet weak var pickerLabel: UILabel!
@@ -112,11 +140,17 @@ class MessagesViewController: MSMessagesAppViewController, UIPickerViewDelegate,
          */
 		pickerView.frame = CGRect(x: 0, y: 30, width: 100, height: 100)
 		pickerView.center = self.view.center
-        pickerView.layer.borderColor = UIColor.purple.cgColor
-        pickerView.layer.borderWidth = 1.0
+		//pickerView.layer.borderColor = UIColor.purple.cgColor
+        //pickerView.layer.borderWidth = 1.0
         rotationAngle = -90 * (.pi/180)
         pickerView.transform = CGAffineTransform(rotationAngle: rotationAngle)
 		
+		let gold = UIColor(hex: "#ffe700ff")
+		writeTextView.layer.borderColor = gold?.cgColor
+		writeTextView.layer.borderWidth = 2.0
+		
+		previewTextView.layer.borderColor = UIColor.blue.cgColor
+		previewTextView.layer.borderWidth = 2.0
     }
 
     func textViewDidBeginEditing(_ textView: UITextView) {
