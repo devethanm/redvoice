add3a68850eb3aad7efeb2928b32b3024ffef88e
Ethan M <ethanmdev@gmail.com>
2020-10-15 13:06:54 -0600

    gearshape added for settings transition button

---
+++ voice MessagesExtension/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  voice MessagesExtension
+//
+//  Created by Ethan Maestas on 10/15/20.
+//  Copyright © 2020 Ethan Maestas. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
